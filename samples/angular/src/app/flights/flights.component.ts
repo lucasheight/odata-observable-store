0b6ce0337194f4807c50c91bf433941fd91122fa
lucasheight <60023789@hneahs.nswhealth.net>
2019-06-04 08:32:30 +1000

    package config changes

--- samples/angular/src/app/flights/flights.component.ts
+++ samples/angular/src/app/flights/flights.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { takeUntil, map } from "rxjs/operators";
+import { FlightsService } from '../services/flights.service';
+import { IFlight } from '../services/IFlight';
 
 @Component({
   selector: 'app-flights',
   templateUrl: './flights.component.html',
   styles: []
 })
-export class FlightsComponent implements OnInit {
-
-  constructor() { }
+export class FlightsComponent implements OnInit, OnDestroy {
+  destroy$: Subject<void> = new Subject();
+  flights$: Observable<IFlight[]>;
+  constructor(private flightService: FlightsService) { }
 
   ngOnInit() {
+    //this.flights$ = this.flightService.state$;
+
+  }
+  ngOnDestroy() {
+    this.destroy$.next();
   }
 
 }
